6908dcc7edfa6d4565edfa777e57e1c04c733d22
Cameron Paul <cbp12231999@gmail.com>
2026-01-13 17:02:26 -0600

    More file relocation and module compile

--- src/apps/cooker_main.zig
+++ src/apps/cooker_main.zig
@@ -1,1 +1,19 @@
 const std = @import("std");
+
+
+// GOAL is to cook png files and then add to renderer
+pub fn main() void {
+
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    _ = allocator;
+
+    std.log.info("asset cooker has started", .{});
+
+
+    
+
+
+}
