c9cb640f85ce10fb6ac65cbe6786eb91023f10be
Cameron-Paul99 <154998243+Cameron-Paul99@users.noreply.github.com>
2026-02-07 14:02:29 -0600

    Skeletal ECS and Game exe (#6)
    * start of ecs system
    
    * two bit stuff
    
    * game exe and versioning

--- src/apps/game_main.zig
+++ src/apps/game_main.zig
@@ -1,1 +1,29 @@
 const std = @import("std");
+const engine = @import("engine");
+
+pub fn main() !void {
+    
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    var game_window = try engine.sdl.Window.init(800, 600);
+    defer game_window.deinit();
+
+    var core = try engine.core.Core.init(true, allocator, &game_window);
+    defer core.deinit(allocator);
+
+    var swapchain = try engine.swapchain.Swapchain.init(allocator, &core, &game_window, .{.vsync = false}, null);
+    defer swapchain.deinit(&core, allocator, core.alloc_cb);
+
+    var renderer = try engine.renderer.Renderer.init(allocator, &core, &swapchain, &game_window);
+    defer renderer.deinit(allocator, &core);
+
+    while (!game_window.should_close){
+        
+        game_window.pollEvents(&renderer);
+
+    }
+
+
+}
