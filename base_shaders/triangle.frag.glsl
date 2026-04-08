fba27dcc9e602b0a94581e3dc05da7435b6fe164
Cameron Paul <cbp12231999@gmail.com>
2026-04-08 10:03:01 -0500

    shader update

--- base_shaders/triangle.frag.glsl
+++ base_shaders/triangle.frag.glsl
@@ -3,7 +3,7 @@
 layout(set = 1, binding = 0) uniform sampler atlasSampler;
 layout(set = 1, binding = 1) uniform texture2D atlases[64];
 
-layout(location = 0) in vec3 frag_color;
+layout(location = 0) in vec4 frag_color;
 layout(location = 1) in vec2 frag_tex_coord;
 layout(location = 2) flat in uint frag_atlas_id;
 
@@ -12,10 +12,10 @@ layout(location = 0) out vec4 outColor;
 
 void main(){
 
-	outColor = texture(
-        	sampler2D(atlases[frag_atlas_id], atlasSampler),
-        	frag_tex_coord
-    	);
-	//outColor = vec4(frag_color * texture(texSampler, frag_tex_coord).rgb, 1.0);
+    vec4 tex = texture(
+        sampler2D(atlases[frag_atlas_id], atlasSampler),
+        frag_tex_coord
+    );
 
+    outColor = frag_color * tex;
 }
